91ed8c165582ba9c2f40a9aad42a3f44a1f15c42
sherzock <jacobo.galofre@gmail.com>
2024-06-02 00:44:46 +0200

    Added Post for Courses

--- src/courses.ts
+++ src/courses.ts
@@ -15,4 +15,23 @@ coursesRouter.get("/", async (req, res) => {
   }
 });
 
+coursesRouter.post("/", async (req, res) => {
+  try {
+    const { name, code , teacherId, classId } = req.body;
+
+    if (name === undefined || typeof name !== "string") {
+      return res.status(400).json({ error: "Missing `name` field " });
+    }
+    const course = await db.course.create({
+      data: { name: name, code: code, teacherId: teacherId, classromId: classId },
+    });
+    res.status(201).json(course);
+  } catch (e) {
+    console.error(e);
+    res
+      .status(500)
+      .json({ error: "Couldn't create Course, come back later..." });
+  }
+});
+
 export default coursesRouter;

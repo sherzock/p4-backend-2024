bee12e88451843f1a3a88da093dc640046e5bf08
sherzock <jacobo.galofre@gmail.com>
2024-06-01 20:08:36 +0200

    Eliminated index.ts and started server.ts with first basic root endpoint that returns status 200

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.status(200).json({ok: true, message: "Hello"});
+})
+
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+    console.log(`The server is listening on http://localhost:${PORT}`);
+});

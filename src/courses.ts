3d1cfde251729c3d3f6a679a2c32c6221b787df8
sherzock <jacobo.galofre@gmail.com>
2024-06-02 01:11:52 +0200

    Added get by id endpoint for courses

--- src/courses.ts
+++ src/courses.ts
@@ -5,11 +5,11 @@ const coursesRouter = Router();
 
 coursesRouter.get("/", async (req, res) => {
   try {
-    const Courses = await db.course.findMany({
+    const courses = await db.course.findMany({
       orderBy: { name: "asc" },
     });
 
-    res.status(200).json(Courses);
+    res.status(200).json(courses);
   } catch (e) {
     res.status(500).json({ error: "Internal Error" });
   }
@@ -17,13 +17,18 @@ coursesRouter.get("/", async (req, res) => {
 
 coursesRouter.post("/", async (req, res) => {
   try {
-    const { name, code , teacherId, classId } = req.body;
+    const { name, code, teacherId, classId } = req.body;
 
     if (name === undefined || typeof name !== "string") {
       return res.status(400).json({ error: "Missing `name` field " });
     }
     const course = await db.course.create({
-      data: { name: name, code: code, teacherId: teacherId, classromId: classId },
+      data: {
+        name: name,
+        code: code,
+        teacherId: teacherId,
+        classromId: classId,
+      },
     });
     res.status(201).json(course);
   } catch (e) {
@@ -31,6 +36,25 @@ coursesRouter.post("/", async (req, res) => {
     res
       .status(500)
       .json({ error: "Couldn't create Course, come back later..." });
+  }
+});
+
+coursesRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const course = await db.course.findUniqueOrThrow({
+      where: { idCourse: Number(id) },
+    });
+
+    res.status(200).json(course);
+  } catch (e: any) {
+    if (e.name === "NotFoundError") {
+      return res.status(404).json({ message: `Not found.` });
+    }
+    res
+      .status(500)
+      .json({ error: `Internal error.` });
   }
 });
 
